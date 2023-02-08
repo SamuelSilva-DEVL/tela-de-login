df4f65420fe357bda5d2482d9807410fb0dfdf68
Samuel Silva <samuelmec1@gmail.com>
2023-02-07 23:14:43 -0300

    validação da rota Home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,28 @@
+import { Fragment, ReactNode } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { Registrar } from './pages/CadastrarSe'
+import { SignUp } from './pages/SignUp'
 import { HomePage } from './pages/Home'
-import { SingUp } from './pages/Login'
+import { SignIn } from './pages/SignIn'
+import useAuth from './hooks/useAuth'
+
+const Private = () => {
+   const { signed } = useAuth();
+
+   return signed > 0 ? <HomePage /> : <SignIn />
+}
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SingUp />}/>
-        <Route path='/register' element={<Registrar />}/>
-        <Route path='/home' element={<HomePage />}/>
-      </Routes>
+      <Fragment>
+        <Routes>
+          <Route path='/' element={<SignIn />}/>
+          <Route path='/register' element={<SignUp />}/>
+          {/* <Route path='/home' element={<HomePage/>}/> */}
+          <Route path='/home' element={<Private />}/>
+          <Route path='*' element={<SignIn />} />
+        </Routes>
+      </Fragment>
     </BrowserRouter>
   )
 }
