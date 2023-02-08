f71b5859bb17e420d32c2e1dc36091a5536618a7
Samuel Silva <samuelmec1@gmail.com>
2023-02-07 23:11:25 -0300

    Criação contexto e hook para consumir o contexto

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,22 +1,38 @@
-import {createContext, ReactNode, useEffect, useState} from 'react'
+import { createContext, ReactNode, useEffect, useState } from "react";
 
-interface IProps {
-  children: ReactNode,
+interface IUser {
+  name?: string 
+  email: string
+  password?: string
 }
 
-interface ISingIn {
-  email: string,
+interface IContextProps{
+  user: IUser | null
+  signin: (email: string, password: string) => string | boolean 
+  signup: (email: string, password: string, name: string) => string | boolean
+  signout: () => void
+  signed: any
+}
+
+export const AuthContext = createContext<IContextProps>(null!);
+
+interface IAuthProps{
+  children: ReactNode
+}
+
+interface IAuthProviderProps {
+  email: string
+  password: string
+  name: string
+}
+
+interface IUserProps {
+  email: string
   password: string
 }
 
-type UsersProps = {
-  email: string
-}
-
-export const AuthContext = createContext({})
-
-export const AuthProvider = ({children}: IProps) => {
-  const [user, setUser] = useState<UsersProps[]>()
+export const AuthProvider = ({ children }: IAuthProps) => {
+  const [user, setUser] = useState<IUser | null>(null);
 
   useEffect(() => {
     const userToken = localStorage.getItem("user_token");
@@ -24,22 +40,67 @@ export const AuthProvider = ({children}: IProps) => {
 
     if (userToken && usersStorage) {
       const hasUser = JSON.parse(usersStorage)?.filter(
-        (user) => user.email === JSON.parse(userToken).email
+        (user: IUserProps) => user.email === JSON.parse(userToken).email
       );
 
       if (hasUser) setUser(hasUser[0]);
     }
   }, []);
 
-  const SignIn = ({email, password}: ISingIn) =>{
+  const signin = (email: string, password: string) => {
+    //@ts-ignore
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+    
+    const hasUser = usersStorage?.filter((user: IUserProps) => user.email === email);
 
-  }
+    if (hasUser?.length) {
+      if (hasUser[0].email === email && hasUser[0].password === password) {
+        const name = hasUser[0].name;
+        const token = Math.random().toString(36).substring(2);
+        localStorage.setItem("user_token", JSON.stringify({ email, token }));
+        setUser({ email, name });
+        return true;
+      } else {
+        return "E-mail ou senha incorretos";
+      }
+    } else {
+      return "Usuário não cadastrado";
+    }
+  };
 
-  return(
-    <AuthContext.Provider value={{
+  const signup = (email: string, password: string, name: string) => {
+    //@ts-ignore
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
 
-    }}>
+    const hasUser = usersStorage?.filter((user: IUserProps) => user.email === email);
+
+    if (hasUser?.length) {
+       return "Já tem uma conta com esse E-mail";
+    }
+
+    let newUser;
+
+    if (usersStorage) {
+      newUser = [...usersStorage, { email, password, name }];
+    } else {
+      newUser = [{ email, password, name }];
+    }
+
+     localStorage.setItem("users_bd", JSON.stringify(newUser));
+
+    return true;
+  };
+
+  const signout = () => {
+    setUser({email: " ", name: " "});
+    localStorage.removeItem("user_token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signin, signup, signout, signed: !!user}}
+    >
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
