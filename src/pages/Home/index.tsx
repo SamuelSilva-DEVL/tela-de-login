e5685ecc724c6dfb5850f49706cb48aa3b3601ed
Samuel Silva <samuelmec1@gmail.com>
2023-02-07 23:13:07 -0300

    Criação pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,77 @@
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useState } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { ListToDo } from "../../components/ListToDo"
+import ToDoFormSchema from "../../validations/validationToDo"
+import { Button, Container, ContainerList, Form, Input, TextError } from "./styles"
+import { GrAddCircle } from 'react-icons/gr'
+import useAuth from "../../hooks/useAuth"
+import { useNavigate } from "react-router-dom"
+
 interface IHomeProps{
   nome?: string,
   sobrenome?: string
 }
 
-export function HomePage({nome, sobrenome}: IHomeProps){
-  return <h1>Bem vindo: {nome + ' ' +sobrenome}</h1>
+interface IListProps {
+  name: string
+  id: number
+}
+
+export function HomePage(){
+  const { signout, user } = useAuth()
+  const navigate = useNavigate()
+  const [items, setItems] = useState<IListProps []>([])
+  const {
+    register, 
+    handleSubmit, 
+    formState: {errors}, 
+    reset 
+  } = useForm<IListProps>({resolver: yupResolver(ToDoFormSchema)})
+
+  const addListItem: SubmitHandler<IListProps> 
+    = async  (value) => {
+    var newList = [...items]
+    newList.push({
+      id: items.length + 1,
+      name: value.name
+    })
+
+    setItems(newList)
+    reset()
+  }
+
+  const deleteListItem = (id: number) => {
+     var filter = items.filter((items) => items.id != id)
+     setItems(filter)
+  }
+
+  return (
+    <>
+    <h1>Bem vindo {user?.name}</h1>
+      <button onClick={() => [signout(), navigate("/")]}>Sair</button>
+      <Container>
+        <h1>To Do List</h1>
+
+        {errors?.name && <TextError>* {errors.name.message}</TextError>}
+
+        <Form onSubmit={handleSubmit(addListItem)}>
+          <Input type="text" {...register("name")} placeholder="Crie uma tarefa"/>
+          <Button type="submit"><GrAddCircle/></Button>
+        </Form>
+
+        <ContainerList>
+          {items?.map((item, id) => (
+            <ListToDo 
+              key={id} 
+              description={item.name}
+              list={item}
+              onClick={deleteListItem}
+            />
+          ))}
+        </ContainerList>
+      </Container>  
+    </>
+  )
+  
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.azul};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 5px;
+`
+
+export const ContainerList = styled.div`
+  min-height: 100px;
+  width: 600px;
+`
+export const Input = styled.input`
+  width: 200px;
+  height: 40px;
+  font-size: 16px;
+  border: none;
+
+  :focus{
+    outline: 0;
+  }
+`
+export const Button = styled.button`
+  background: none;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+  font-size: 36px;
+`
+
+export const TextError = styled.p`
+  color: red;
+`
